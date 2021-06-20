a44f5052d91e3649f238cbbc6c28199859dfbcac
Yanis <62204811+YanisAhdjoudj@users.noreply.github.com>
2021-06-20 06:49:41 +0200

    new_data

--- 3-Programmes/1_Préparation_des_données.py
+++ 3-Programmes/1_Préparation_des_données.py
@@ -12,36 +12,42 @@ import numpy as np
 import os
 
 path = os.path.abspath(os.path.join(os.path.dirname( os.getcwd() ), '.'))
-<<<<<<< HEAD
+
 path = r"C:\Users\yanis\01 Projets\01 Python Projects\Projet_Finance_Quant\Projet-Finance-Quant"
 os.chdir(path+"\\3-Programmes")
-=======
-#os.chdir(path+"\\3-Programmes")
->>>>>>> 167fade9abeb06bbc79cdbe61c09733216c0e087
+
 from Utils.utils_finance import get_stock,get_multiple_stock,get_returns
+
+
+
+
+
+
+
 
 ########################## Importation des données ##########################
 
-# Nous disposons de données pour 26 entreprises européeenne 
+# Nous disposons de données pour 46 entreprises européeenne 
 # coté chez euronext
 
-# Importation des données ESG des 26 entreprises
+# Importation des données ESG des 46 entreprises
 
-df_ESG=pd.read_excel(path+"/2-Données/data_fi.xls")
-
-
-# Importation des rendements annuels des 26 entreprises
-
-df_yearly_returns=pd.read_excel(path+"/2-Données/yearly_returns.xls")
+df_ESG=pd.read_excel(path+"/2-Données/Raw_data/ESG_data.xls")
 
 
+# Importation des rendements annuels des 46 entreprises
 
-# Importation des données boursieres des 26 entreprises
+df_yearly_returns=pd.read_excel(path+"/2-Données/Raw_data/yearly_returns.xls")
+
+
+# Importation des données boursieres des 46 entreprises
 
 ticker_list=["ALV.DE","KNEBV.HE","KER.PA","FP.PA","VOW3.DE","SAN.PA","SU.PA","ENEL.MI","ISP.MI","INGA.AS","BAYN.DE","IBE.MC",
              "MC.PA","CRH.L","SIE.DE","DAI.DE","LIN.DE","ENGI.PA","AI.PA","ENI.MI","PHIA.AS","MUV2.DE","BAS.DE","BMW.DE","OR.PA","RI.PA"]
+ 
+# a completer 
 
-start_date= "2005-01-01"
+start_date= "2004-01-01"
 end_date="2020-01-01"
 
 df_stocks=get_multiple_stock(tickers=ticker_list,start=start_date, end=end_date , value = 'Open',index_as_date=True)
@@ -55,25 +61,68 @@ df_stocks_returns=get_returns(df_stocks)
 df_benchmark=get_multiple_stock(["^STOXX50E","^N100","^GSPC"],start=start_date, end=end_date, value="Open", index_as_date=True)
 df_benchmark_returns=get_returns(df_benchmark)
 
+# Importation des données retravaillé et annualisé 
+df_benchmark_yearly=pd.read_excel(path+"/2-Données/Raw_data/benchmark_yearly.xls")
+df_benchmark_yearly_returns=pd.read_excel(path+"/2-Données/Raw_data/benchmark_yearly_returns.xls")
+
+
+
+
+
+
+
 
 
 ########################## Préparation des données ##########################
 
 # Jointure entre les données ESG et les rendements annuels
 
-main_df = pd.merge(df_ESG, df_yearly_returns , how='left' , on = ['date','Entreprises'])
+df_1 = pd.merge(df_ESG, df_yearly_returns , how='left' , on = ['date','Entreprises'])
+
+
+# Jointure entre les données et les rendements annuels des benchmarks
+
+# Retraitement des dates
+df_1["year"]=pd.DatetimeIndex(df_1['date']).year
+df_benchmark_yearly_returns["year"]=(pd.DatetimeIndex(df_benchmark_yearly_returns['Date']).year)-1
+#Jointure
+df = pd.merge(df_1, df_benchmark_yearly_returns , how='left' , on = ['year'])
+
+# gestion des colonnes
+
+df.drop(["date","year","Date"], inplace=True, axis=1)
+
 
 # Creation de la variable cible 
-niveaux_notes=pd.cut(main_df['yearly_return'],bins=[-25, 15],labels=['Long', 'Mid', 'Short'])
 
-main_df['target'] = main_df.apply(lambda x: 1 if x['yearly_return']<15 else if x['yearly_return'], axis=1)
+# Voir nass
+
+
+
+
+# Calcul des taux de croissance par variable
+# Pas fini
+
+var_list_growth=[""]
+
+for var in var_list_growth :
+    
+    df[var+"_GROWTH"] = df[var].pct_change()
+    
+ 
 
 
 # Création des dummys pour savoir si il y'a une valeur manquantes
 
-main_df['dummy1'] = (pd.to_numeric(main_df.NUMBER_EMPLOYEES_CSR, errors='coerce').notnull() > 0).astype('int')
+var_list_miss=[""]
+
+for var in var_list_miss :
+    
+    df[var+"_MISS"] = (pd.to_numeric(df[var], errors='coerce').notnull() > 0).astype('int')
+ 
+
 ########################## Exportation des données ##########################
 
-df_stocks.to_csv(os.path.join(path, '2-Données' , 'stocks_data.csv'),sep=';',header=True,index=True)
-main_df.to_csv(os.path.join(path, '2-Données' , 'data.csv'),sep=';',header=True,index=False)
+#df_stocks.to_csv(os.path.join(path, '2-Données' , 'stocks_data.csv'),sep=';',header=True,index=True)
+df.to_csv(os.path.join(path, '2-Données' , 'data.csv'),sep=';',header=True,index=False)
 
